0d1b694e3ef683f1dd9f9e217122d49c9a1e85ed
Kostya Kaganov <kkaganov@gmail.com>
2021-02-06 19:38:46 +0200

    Add routing to App component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,44 @@
 import React, {Component} from 'react';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import Main from '../main/main';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import Film from '../film/film';
+import AddReview from '../add-review/add-review';
+import Player from '../player/player';
+import NotFound from '../not-found/not-found';
 import PropTypes from 'prop-types';
 
 export default class App extends Component {
   render() {
     const {genre, year} = this.props;
-    return <Main genre={genre} year={year} />;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main genre={genre} year={year} />
+          </Route>
+          <Route exact path="/login">
+            <SignIn />
+          </Route>
+          <Route exact path="/mylist">
+            <MyList />
+          </Route>
+          <Route exact path="/films/:id">
+            <Film />
+          </Route>
+          <Route exact path="/films/:id/review">
+            <AddReview />
+          </Route>
+          <Route exact path="/player/:id">
+            <Player />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
