1219310af277cbfa702bd4a65af231ad10625abd
Kostya Kaganov <kkaganov@gmail.com>
2021-02-06 20:13:25 +0200

    Replace anchor element with Link element in AddReview component

--- src/components/add-review/add-review.jsx
+++ src/components/add-review/add-review.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 export default class AddReview extends Component {
   render() {
@@ -13,11 +14,11 @@ export default class AddReview extends Component {
 
           <header className="page-header">
             <div className="logo">
-              <a href="main.html" className="logo__link">
+              <Link to="/" className="logo__link">
                 <span className="logo__letter logo__letter--1">W</span>
                 <span className="logo__letter logo__letter--2">T</span>
                 <span className="logo__letter logo__letter--3">W</span>
-              </a>
+              </Link>
             </div>
 
             <nav className="breadcrumbs">
