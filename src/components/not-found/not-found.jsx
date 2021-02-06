065ddceb0ae10ae5a7b6fe8deec08799029359e1
Kostya Kaganov <kkaganov@gmail.com>
2021-02-06 19:15:27 +0200

    Add NotFound component

---
+++ src/components/not-found/not-found.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <div className="user-page">
+        <header className="page-header user-page__head">
+          <div className="logo">
+            <a href="main.html" className="logo__link">Back to Home</a>
+          </div>
+          <h1 className="page-title user-page__title">404. Page Not Found</h1>
+        </header>
+        <footer className="page-footer">
+          <div className="logo">
+            <a href="main.html" className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
