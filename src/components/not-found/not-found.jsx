ce635b87d2e291591c0e59bed73015174be163c6
Kostya Kaganov <kkaganov@gmail.com>
2021-02-06 20:05:25 +0200

    Replace anchor elements with Link component in NotFound component

--- src/components/not-found/not-found.jsx
+++ src/components/not-found/not-found.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 export default class NotFound extends Component {
   render() {
@@ -6,23 +7,23 @@ export default class NotFound extends Component {
       <div className="user-page">
         <header className="page-header user-page__head">
           <div className="logo">
-            <a href="main.html" className="logo__link">Back to Home</a>
+            <Link to="/" className="logo__link">Back to Home</Link>
           </div>
           <h1 className="page-title user-page__title">404. Page Not Found</h1>
         </header>
         <footer className="page-footer">
           <div className="logo">
-            <a href="main.html" className="logo__link logo__link--light">
+            <Link to="/" className="logo__link logo__link--light">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
-            </a>
+            </Link>
           </div>
           <div className="copyright">
             <p>© 2019 What to watch Ltd.</p>
           </div>
         </footer>
-      </div>
+      </div >
     );
   }
 }
