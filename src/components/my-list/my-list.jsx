7f3c205b51fae1fcb27d34bd47f32392f5f5c486
Kostya Kaganov <kkaganov@gmail.com>
2021-02-06 20:19:38 +0200

    Replace anchor element with Link element in MyList component

--- src/components/my-list/my-list.jsx
+++ src/components/my-list/my-list.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 export default class MyList extends Component {
   render() {
@@ -6,11 +7,11 @@ export default class MyList extends Component {
       <div className="user-page">
         <header className="page-header user-page__head">
           <div className="logo">
-            <a href="main.html" className="logo__link">
+            <Link to="/" className="logo__link">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
-            </a>
+            </Link>
           </div>
 
           <h1 className="page-title user-page__title">My list</h1>
@@ -113,11 +114,11 @@ export default class MyList extends Component {
 
         <footer className="page-footer">
           <div className="logo">
-            <a href="main.html" className="logo__link logo__link--light">
+            <Link to="/" className="logo__link logo__link--light">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
-            </a>
+            </Link>
           </div>
 
           <div className="copyright">
