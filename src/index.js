0f7b5ae190aad1485692f7b3deb590008cd46c76
Kostya Kaganov <kkaganov@gmail.com>
2021-02-10 21:10:18 +0200

    Pass film and reviews mocks to App props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import films from './mocks/films';
+import reviews from './mocks/reviews';
 
 const MOVIE_GENRE = `Comedy`;
 const MOVIE_YEAR = 2019;
 
 ReactDOM.render(
-    <App genre={MOVIE_GENRE} year={MOVIE_YEAR}/>,
+    <App genre={MOVIE_GENRE} year={MOVIE_YEAR} films={films} reviews={reviews}/>,
     document.querySelector(`#root`)
 );
