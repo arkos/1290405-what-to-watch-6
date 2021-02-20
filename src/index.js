4e1ad0ad3d29ac6fab1ae2c5c92f625b1bef7b5e
Kostya Kaganov <kkaganov@gmail.com>
2021-02-20 20:03:57 +0200

    Link redux to the project, initialize store, add Provider and pass store to it

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import films from './mocks/films';
 import reviews from './mocks/reviews';
+import {reducer} from './store/reducer';
 
 const MOVIE_GENRE = `Comedy`;
 const MOVIE_YEAR = 2019;
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App genre={MOVIE_GENRE} year={MOVIE_YEAR} films={films} reviews={reviews}/>,
+    <Provider store={store}>
+      <App
+        genre={MOVIE_GENRE}
+        year={MOVIE_YEAR}
+        films={films}
+        reviews={reviews}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
