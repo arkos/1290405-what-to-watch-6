b94c63b794e5e653395115fce99b2cdd5cd76110
Kostya Kaganov <kkaganov@gmail.com>
2021-02-20 19:56:12 +0200

    Add action to select a genre and filter movies by genre, create reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,24 @@
+import {ActionType} from '../store/action';
+
+const initialState = {
+  genre: `all`,
+  movies: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_GENRE:
+      return {
+        ...state,
+        genre: action.payload
+      };
+    case ActionType.GET_MOVIES:
+      return {
+        ...state,
+        movies: [...state.movies]
+      };
+    default: return state;
+  }
+};
+
+export {reducer};
