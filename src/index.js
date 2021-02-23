ca6b20c75b03a3fd673bb673756f4b16cd4e1a95
Kostya Kaganov <kkaganov@gmail.com>
2021-02-23 22:23:48 +0200

    Link redux-thunk middleware to store, create api and perform authorization check

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import {reducer} from './store/reducer';
+import {AuthorizationStatus} from './const';
+import {ActionCreator} from './store/action';
+import {createAPI} from './services/api';
+import {checkAuth} from './store/api-actions';
 import App from './components/app/app';
+
+const api = createAPI(
+    () => store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH))
+);
 
 const store = createStore(
     reducer,
-    composeWithDevTools()
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
+
+store.dispatch(checkAuth());
 
 ReactDOM.render(
     <Provider store={store}>
