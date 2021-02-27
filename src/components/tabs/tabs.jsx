10b6cd04e6e6f92ac4ac2205eea376c067b8fd74
Kostya Kaganov <kkaganov@gmail.com>
2021-02-27 23:58:12 +0200

    Add Tabs component

---
+++ src/components/tabs/tabs.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import OverviewTab from '../overview-tab/overview-tab';
+
+const Tabs = () => {
+  return (
+    <div className="movie-card__info">
+      <div className="movie-card__poster movie-card__poster--big">
+        <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+      </div>
+
+      <div className="movie-card__desc">
+        <nav className="movie-nav movie-card__nav">
+          <ul className="movie-nav__list">
+            <li className="movie-nav__item movie-nav__item--active">
+              <a href="#" className="movie-nav__link">Overview</a>
+            </li>
+            <li className="movie-nav__item">
+              <a href="#" className="movie-nav__link">Details</a>
+            </li>
+            <li className="movie-nav__item">
+              <a href="#" className="movie-nav__link">Reviews</a>
+            </li>
+          </ul>
+        </nav>
+        <OverviewTab />
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
