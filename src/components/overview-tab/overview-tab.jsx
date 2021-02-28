8185843a632d8b531de97e5e24c8aa004015dd4b
Kostya Kaganov <kkaganov@gmail.com>
2021-02-28 08:35:29 +0200

    Replace plain text with movie properties

--- src/components/overview-tab/overview-tab.jsx
+++ src/components/overview-tab/overview-tab.jsx
@@ -1,26 +1,31 @@
 import React, {Fragment} from 'react';
+import Validator from '../../validate';
 
-const OverviewTab = () => {
+const OverviewTab = ({movie}) => {
+  const {rating, score, director, starring, description} = movie;
+
   return (
     <Fragment>
       <div className="movie-rating">
-        <div className="movie-rating__score">8,9</div>
+        <div className="movie-rating__score">{rating}</div>
         <p className="movie-rating__meta">
           <span className="movie-rating__level">Very good</span>
-          <span className="movie-rating__count">240 ratings</span>
+          <span className="movie-rating__count">{`${score} ratings`}</span>
         </p>
       </div>
       <div className="movie-card__text">
-        <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.</p>
+        <p>{description}</p>
 
-        <p>Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
+        <p className="movie-card__director"><strong>{`Director: ${director}`}</strong></p>
 
-        <p className="movie-card__director"><strong>Director: Wes Andreson</strong></p>
-
-        <p className="movie-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other</strong></p>
+        <p className="movie-card__starring"><strong>{`Starring: ${starring.join(`, `)} and other`}</strong></p>
       </div>
     </Fragment>
   );
+};
+
+OverviewTab.propTypes = {
+  movie: Validator.MOVIE
 };
 
 export default OverviewTab;
