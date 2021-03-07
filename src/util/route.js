63a60b283f72d543ebb68c1557ee050837012527
Kostya Kaganov <kkaganov@gmail.com>
2021-03-07 13:27:11 +0200

    Fix movie preview URL

--- src/util/route.js
+++ src/util/route.js
@@ -2,6 +2,6 @@ import {AppRoute} from './const';
 
 export const getMovieUrl = (id = `:id`) => `${AppRoute.MOVIES}/${id}`;
 
-export const getReviewUrl = (id = `:id`) => `${AppRoute.MOVIES}/review/${id}`;
+export const getReviewUrl = (id = `:id`) => `${AppRoute.MOVIES}/${id}/review`;
 
 export const getPlayerUrl = (id = `:id`) => `/player/${id}`;
