a4582cecfd8bacf2696145ca0fdcf45f480842e5
Kostya Kaganov <kkaganov@gmail.com>
2021-03-07 10:29:31 +0200

    Add route test for SignIn

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -5,12 +5,30 @@ import * as redux from 'react-redux';
 import {createMemoryHistory} from 'history';
 import {render, screen} from '@testing-library/react';
 import App from '../app/app';
+import {AppRoute} from '../../util/const';
 
 const mockStore = configureStore({});
 
 describe(`Test routing`, () => {
   jest.spyOn(redux, `useSelector`);
   jest.spyOn(redux, `useDispatch`);
+
+  it(`Render SignIn when user navigates to /login`, () => {
+    const history = createMemoryHistory();
+    history.push(AppRoute.LOGIN);
+
+    render(
+        <redux.Provider store={mockStore({})}>
+          <Router history={history}>
+            <App />
+          </Router>
+        </redux.Provider>
+    );
+
+    expect(screen.getAllByText(/Sign in/i)).toHaveLength(2);
+    expect(screen.getByPlaceholderText(/Email address/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Password/i)).toBeInTheDocument();
+  });
 
   it(`Render NotFound when user navigates to non-existing route`, () => {
     const history = createMemoryHistory();
