2a4548dffe0dfadef27214b9dec6ff7bcf3eb749
Kostya Kaganov <kkaganov@gmail.com>
2021-03-07 10:31:52 +0200

    Remove obsolete action getMovies

--- src/store/action.js
+++ src/store/action.js
@@ -3,7 +3,6 @@ import {createAction} from '@reduxjs/toolkit';
 
 export const ActionType = {
   CHANGE_GENRE: `movies/changeGenre`,
-  GET_MOVIES: `movies/getMovies`,
   LOAD_MOVIES: `data/loadMovies`,
   REQUIRE_AUTHORIZATION: `user/requiredAuthorization`,
   REDIRECT_TO_ROUTE: `user/redirectToRoute`
@@ -14,8 +13,6 @@ export const changeGenre = createAction(ActionType.CHANGE_GENRE, (genre) => {
     payload: genre
   };
 });
-
-export const getMovies = createAction(ActionType.GET_MOVIES);
 
 export const loadMovies = createAction(ActionType.LOAD_MOVIES, (movies) => {
   return {
