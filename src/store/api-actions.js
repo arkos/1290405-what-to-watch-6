08ab9168bbbd82d85975d4e556f8cfc770a421b4
Kostya Kaganov <kkaganov@gmail.com>
2021-03-10 21:56:04 +0200

    Preserve the same review structure as on server

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,13 +1,12 @@
 import {loadMovies, loadReviews, requireAuthorization, redirectToRoute} from '../store/action';
 import {AuthorizationStatus} from '../util/const';
-import {adaptToClient as adaptMovieToClient} from '../util/movie';
-import {adaptToClient as adaptReviewToClient} from '../util/review';
+import {adaptToClient} from '../util/movie';
 import {APIRoute} from '../util/const';
 import {getApiReviewsUrl} from '../util/route';
 
 export const fetchMovies = () => (dispatch, _getState, api) => (
   api.get(APIRoute.MOVIES)
-    .then(({data}) => dispatch(loadMovies(data.map(adaptMovieToClient))))
+    .then(({data}) => dispatch(loadMovies(data.map(adaptToClient))))
 );
 
 export const checkAuth = () => (dispatch, _getState, api) => (
@@ -24,5 +23,5 @@ export const login = ({login: email, password}) => (dispatch, _getState, api) =>
 
 export const fetchReviews = ({id: movieId}) => (dispatch, _getState, api) => (
   api.get(getApiReviewsUrl(movieId))
-    .then(({data}) => dispatch(loadReviews(data.map(adaptReviewToClient), movieId)))
+    .then(({data}) => dispatch(loadReviews(data, movieId)))
 );
