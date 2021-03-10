3041ef82b4ad330a57962126eeeace29fb3875b6
Kostya Kaganov <kkaganov@gmail.com>
2021-03-10 21:54:07 +0200

    Rename review.js to common.js

--- src/util/review.js
+++
@@ -1,7 +0,0 @@
-export const adaptToClient = (review) => {
-  const adaptedReview = Object.assign({}, review, {
-    date: review.date !== null ? new Date(review.date) : null
-  });
-
-  return adaptedReview;
-};
