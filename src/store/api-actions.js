ed36c060bac8a78664a653e6b973234a3ff53679
Kostya Kaganov <kkaganov@gmail.com>
2021-03-10 19:02:56 +0200

    Add API action to fetch reviews

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,11 +1,13 @@
-import {loadMovies, requireAuthorization, redirectToRoute} from '../store/action';
+import {loadMovies, loadReviews, requireAuthorization, redirectToRoute} from '../store/action';
 import {AuthorizationStatus} from '../util/const';
-import {adaptToClient} from '../util/movie';
+import {adaptToClient as adaptMovieToClient} from '../util/movie';
+import {adaptToClient as adaptReviewToClient} from '../util/review';
 import {APIRoute} from '../util/const';
+import {getApiReviewsUrl} from '../util/route';
 
 export const fetchMovies = () => (dispatch, _getState, api) => (
   api.get(APIRoute.MOVIES)
-    .then(({data}) => dispatch(loadMovies(data.map(adaptToClient))))
+    .then(({data}) => dispatch(loadMovies(data.map(adaptMovieToClient))))
 );
 
 export const checkAuth = () => (dispatch, _getState, api) => (
@@ -18,4 +20,9 @@ export const login = ({login: email, password}) => (dispatch, _getState, api) =>
   api.post(APIRoute.LOGIN, {email, password})
     .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
     .then(() => dispatch(redirectToRoute(APIRoute.ROOT)))
+);
+
+export const fetchReviews = ({id: movieId}) => (dispatch, _getState, api) => (
+  api.get(getApiReviewsUrl(movieId))
+    .then(({data}) => dispatch(loadReviews(data.map(adaptReviewToClient), movieId)))
 );
