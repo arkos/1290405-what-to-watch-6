31d7b1b5fb6b54ff931eb93053eec750d7d6076c
Kostya Kaganov <kkaganov@gmail.com>
2021-03-10 19:01:56 +0200

    Add loadReviews to movieData reducer

--- src/store/movie-data/movie-data.js
+++ src/store/movie-data/movie-data.js
@@ -1,16 +1,23 @@
 import {createReducer} from '@reduxjs/toolkit';
-import {loadMovies} from '../action';
+import {loadMovies, loadReviews} from '../action';
 
 const initialState = {
   movies: [],
   userMovies: [],
-  isDataLoaded: false
+  isDataLoaded: false,
+  reviewsLoaded: []
 };
 
 const movieData = createReducer(initialState, (builder) => {
   builder.addCase(loadMovies, (state, action) => {
     state.movies = action.payload;
     state.isDataLoaded = true;
+  });
+
+  builder.addCase(loadReviews, (state, action) => {
+    const movieToFind = state.movies.find((movie) => movie.id === action.payload.movieId);
+    movieToFind.reviews = action.payload.reviews;
+    state.reviewsLoaded.push(action.payload.movieId);
   });
 });
 
