81b8fdd25d5d36fd7b4ba4cacbd6fa75b7b64fe5
Kostya Kaganov <kkaganov@gmail.com>
2021-03-10 21:52:22 +0200

    Fix util for getting review API url

--- src/util/route.js
+++ src/util/route.js
@@ -6,4 +6,4 @@ export const getReviewUrl = (id = `:id`) => `${AppRoute.MOVIES}/${id}/review`;
 
 export const getPlayerUrl = (id = `:id`) => `/player/${id}`;
 
-export const getApiReviewsUrl = (id) => `${APIRoute}/${id}`;
+export const getApiReviewsUrl = (id) => `${APIRoute.REVIEWS}/${id}`;
