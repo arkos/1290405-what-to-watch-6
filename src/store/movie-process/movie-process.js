1713e9b453a681665b3ae8627626354c2c73e330
Kostya Kaganov <kkaganov@gmail.com>
2021-03-11 21:22:37 +0200

    Add changeActiveTab action to movieProcess reducer

--- src/store/movie-process/movie-process.js
+++ src/store/movie-process/movie-process.js
@@ -1,14 +1,19 @@
 import {createReducer} from "@reduxjs/toolkit";
-import {FILTER_ALL_GENRES} from "../../util/const";
-import {changeGenre} from "../action";
+import {FILTER_ALL_GENRES, TabName} from "../../util/const";
+import {changeGenre, changeActiveTab} from "../action";
 
 const initialState = {
-  selectedGenre: FILTER_ALL_GENRES
+  selectedGenre: FILTER_ALL_GENRES,
+  activeTab: TabName.OVERVIEW
 };
 
 const movieProcess = createReducer(initialState, (builder) => {
   builder.addCase(changeGenre, (state, action) => {
     state.selectedGenre = action.payload;
+  });
+
+  builder.addCase(changeActiveTab, (state, action) => {
+    state.activeTab = action.payload;
   });
 });
 
