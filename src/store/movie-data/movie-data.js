64f74e7860e08ae9c2f2d553f46ee60144986144
Kostya Kaganov <kkaganov@gmail.com>
2021-03-11 15:22:17 +0200

    Add reloadMovie to movie-data reducer

--- src/store/movie-data/movie-data.js
+++ src/store/movie-data/movie-data.js
@@ -1,5 +1,5 @@
 import {createReducer} from '@reduxjs/toolkit';
-import {loadMovies, loadReviews} from '../action';
+import {loadMovies, reloadMovie, loadReviews} from '../action';
 
 const initialState = {
   movies: [],
@@ -18,6 +18,17 @@ const movieData = createReducer(initialState, (builder) => {
     const movieToFind = state.movies.find((movie) => movie.id === action.payload.movieId);
     movieToFind.reviews = action.payload.reviews;
     state.reviewsLoaded.push(action.payload.movieId);
+  });
+
+  builder.addCase(reloadMovie, (state, action) => {
+    const index = state.movies.findIndex((movie) => movie.id === action.payload.movie.id);
+
+    if (index === -1) {
+      state.movies.splice(0, action.payload.movie);
+      return;
+    }
+
+    state.movies.splice(index, 1, action.payload.movie);
   });
 });
 
