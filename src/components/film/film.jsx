230288b4cef3fe4f48049be1c7e457c3573d6da2
Kostya Kaganov <kkaganov@gmail.com>
2021-03-12 23:42:26 +0200

    Add more like this section to movie page

--- src/components/film/film.jsx
+++ src/components/film/film.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, Fragment} from 'react';
 import {Link, useParams, useHistory} from 'react-router-dom';
 import {useSelector, useDispatch} from 'react-redux';
 import {getAllMovies} from '../../store/selectors/selectors';
@@ -9,6 +9,7 @@ import Tabs from '../tabs/tabs';
 import {fetchMovie} from '../../store/api-actions';
 import Loading from '../loading/loading';
 import {changeActiveTab} from '../../store/action';
+import MovieList from '../movie-list/movie-list';
 
 const Film = () => {
   const {id} = useParams();
@@ -46,57 +47,74 @@ const Film = () => {
   } = movie;
 
   return (
-    <section className="movie-card movie-card--full">
-      <div className="movie-card__hero">
-        <div className="movie-card__bg">
-          <img src={backgroundImagePath} alt={name} />
+    <Fragment>
+      <section className="movie-card movie-card--full">
+        <div className="movie-card__hero">
+          <div className="movie-card__bg">
+            <img src={backgroundImagePath} alt={name} />
+          </div>
+          <h1 className="visually-hidden">WTW</h1>
+          <header className="page-header movie-card__head">
+            <div className="logo">
+              <Link to={AppRoute.ROOT} className="logo__link">
+                <span className="logo__letter logo__letter--1">W</span>
+                <span className="logo__letter logo__letter--2">T</span>
+                <span className="logo__letter logo__letter--3">W</span>
+              </Link>
+            </div>
+            <SignInIndicator />
+          </header>
+          <div className="movie-card__wrap">
+            <div className="movie-card__desc">
+              <h2 className="movie-card__title">{name}</h2>
+              <p className="movie-card__meta">
+                <span className="movie-card__genre">{genre}</span>
+                <span className="movie-card__year">{released}</span>
+              </p>
+              <div className="movie-card__buttons">
+                <button className="btn btn--play movie-card__button" type="button" onClick={() => history.push(`/player/`)}>
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s"></use>
+                  </svg>
+                  <span>Play</span>
+                </button>
+                <button className="btn btn--list movie-card__button" type="button" onClick={() => history.push(`/mylist`)}>
+                  <svg viewBox="0 0 19 20" width="19" height="20">
+                    <use xlinkHref="#add"></use>
+                  </svg>
+                  <span>My list</span>
+                </button>
+                <Link className="btn movie-card__button" to={getReviewUrl(id)}>Add review</Link>
+              </div>
+            </div>
+          </div>
+        </div >
+        <div className="movie-card__wrap movie-card__translate-top">
+          <Tabs movie={movie}/>
         </div>
+      </section >
+      <div className="page-content">
+        <section className="catalog catalog--like-this">
+          <h2 className="catalog__title">More like this</h2>
 
-        <h1 className="visually-hidden">WTW</h1>
+          <MovieList movies={movies}/>
+        </section>
 
-        <header className="page-header movie-card__head">
+        <footer className="page-footer">
           <div className="logo">
-            <Link to={AppRoute.ROOT} className="logo__link">
+            <Link to={AppRoute.ROOT} className="logo__link logo__link--light">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
             </Link>
           </div>
 
-          <SignInIndicator />
-        </header>
-
-        <div className="movie-card__wrap">
-          <div className="movie-card__desc">
-            <h2 className="movie-card__title">{name}</h2>
-            <p className="movie-card__meta">
-              <span className="movie-card__genre">{genre}</span>
-              <span className="movie-card__year">{released}</span>
-            </p>
-
-            <div className="movie-card__buttons">
-              <button className="btn btn--play movie-card__button" type="button" onClick={() => history.push(`/player/${id}`)}>
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s"></use>
-                </svg>
-                <span>Play</span>
-              </button>
-              <button className="btn btn--list movie-card__button" type="button" onClick={() => history.push(`/mylist`)}>
-                <svg viewBox="0 0 19 20" width="19" height="20">
-                  <use xlinkHref="#add"></use>
-                </svg>
-                <span>My list</span>
-              </button>
-              <Link className="btn movie-card__button" to={getReviewUrl(id)}>Add review</Link>
-            </div>
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
           </div>
-        </div>
-      </div >
-
-      <div className="movie-card__wrap movie-card__translate-top">
-        <Tabs movie={movie}/>
+        </footer>
       </div>
-    </section >
+    </Fragment>
   );
 };
 
