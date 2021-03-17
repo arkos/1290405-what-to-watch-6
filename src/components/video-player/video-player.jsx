b568063b1784de1940618a8b74503566351b13c0
Kostya Kaganov <kkaganov@gmail.com>
2021-03-17 21:44:29 +0200

    Move all video player related functionality and controls to VideoPlayer

--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -1,11 +1,25 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useState, useEffect, Fragment} from 'react';
 import PropTypes from 'prop-types';
+import Validator from '../../util/validate';
 
-const VideoPlayer = ({shouldPlay, src, ...restProps}) => {
+const PLAY_BUTTON_WIDTH = 19;
+const PLAY_BUTTON_HEIGHT = 19;
+
+const PAUSE_BUTTON_WIDTH = 14;
+const PAUSE_BUTTON_HEIGHT = 21;
+
+const FULL_SCREEN_BUTTON_WIDTH = 27;
+const FULL_SCREEN_BUTTON_HEIGHT = 21;
+
+const VideoPlayer = ({shouldPlay, movie, isPreview, onPlayButtonClick, ...restProps}) => {
+  const {videoUrl, previewVideoUrl, name} = movie;
+
   const videoRef = useRef();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    videoRef.current.onpause = () => videoRef.current.load();
+    videoRef.current.oncanplaythrough = () => setIsLoading(false);
+    videoRef.current.onpause = isPreview ? () => videoRef.current.load() : () => {};
 
     return () => {
       videoRef.current.pause();
@@ -21,21 +35,68 @@ const VideoPlayer = ({shouldPlay, src, ...restProps}) => {
     }
 
     videoRef.current.pause();
-    videoRef.current.currentTime = 0;
-
   }, [shouldPlay]);
 
+  const createPlayerControlButton = (
+      width,
+      height,
+      xlinkHref,
+      label,
+      className = `player__play`,
+      onButtonClick = onPlayButtonClick) => {
+    return (
+      <button type="button" disabled={`${isLoading ? `disabled` : ``}`} className={className} onClick={onButtonClick}>
+        <svg viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
+          <use xlinkHref={xlinkHref}></use>
+        </svg>
+        <span>{label}</span>
+      </button>
+    );
+  };
+
   return (
-    <video className="player__video" ref={videoRef} {...restProps}>
-      <source src={src}/>
+    <div className={`${!isPreview ? `player` : ``}`}>
+      <video className="player__video" ref={videoRef} {...restProps}>
+        <source src={isPreview ? previewVideoUrl : videoUrl}/>
       Your browser doesn&apos;t support HTML5 video.
-    </video>
+      </video>
+
+      {!isPreview && <Fragment>
+        <button type="button" className="player__exit">Exit</button>
+        <div className="player__controls">
+          <div className="player__controls-row">
+            <div className="player__time">
+              <progress className="player__progress" value="30" max="100"></progress>
+              <div className="player__toggler">Toggler</div>
+            </div>
+            <div className="player__time-value">1:30:29</div>
+          </div>
+          <div className="player__controls-row">
+            {!shouldPlay ?
+              createPlayerControlButton(PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT, `#play-s`, `Play`) :
+              createPlayerControlButton(PAUSE_BUTTON_WIDTH, PAUSE_BUTTON_HEIGHT, `#pause`, `Pause`)
+            }
+            <div className="player__name">{name}</div>
+
+            {createPlayerControlButton(
+                FULL_SCREEN_BUTTON_WIDTH,
+                FULL_SCREEN_BUTTON_HEIGHT,
+                `#full-screen`,
+                `Full screen`,
+                `player__full-screen`
+            )}
+          </div>
+        </div>
+      </Fragment>}
+    </div>
   );
 };
 
 VideoPlayer.propTypes = {
   shouldPlay: PropTypes.bool.isRequired,
-  src: PropTypes.string.isRequired
+  movie: Validator.MOVIE,
+  isPreview: PropTypes.bool.isRequired,
+  onPlayButtonClick: PropTypes.func.isRequired
 };
 
 export default VideoPlayer;
