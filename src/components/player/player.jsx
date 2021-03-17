04a17d3edf30223cfafa78228b99434ebbae68d4
Kostya Kaganov <kkaganov@gmail.com>
2021-03-17 20:36:05 +0200

    Enable play and pause in video player

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useParams} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import {getAllMovies} from '../../store/selectors/selectors';
 import NotFound from '../not-found/not-found';
 import VideoPlayer from '../video-player/video-player';
 
+const PLAY_BUTTON_WIDTH = 19;
+const PLAY_BUTTON_HEIGHT = 19;
+
+const PAUSE_BUTTON_WIDTH = 14;
+const PAUSE_BUTTON_HEIGHT = 21;
+
+const FULL_SCREEN_BUTTON_WIDTH = 27;
+const FULL_SCREEN_BUTTON_HEIGHT = 21;
+
 const Player = () => {
   const {id} = useParams();
+
+  const [isPlaying, setIsPlaying] = useState(true);
 
   const movies = useSelector((state) => getAllMovies(state));
 
@@ -18,9 +29,26 @@ const Player = () => {
 
   const {videoUrl, backgroundImagePath, name} = movie;
 
+  const createPlayerControlButton = (
+      width,
+      height,
+      xlinkHref,
+      label,
+      className = `player__play`,
+      onButtonClick = () => setIsPlaying(!isPlaying)) => {
+    return (
+      <button type="button" className={className} onClick={onButtonClick}>
+        <svg viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
+          <use xlinkHref={xlinkHref}></use>
+        </svg>
+        <span>{label}</span>
+      </button>
+    );
+  };
+
   return (
     <div className="player">
-      <VideoPlayer src={videoUrl} shouldPlay={true} poster={backgroundImagePath} />
+      <VideoPlayer src={videoUrl} shouldPlay={isPlaying} poster={backgroundImagePath} preload="auto" />
 
       <button type="button" className="player__exit">Exit</button>
 
@@ -34,20 +62,19 @@ const Player = () => {
         </div>
 
         <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use xlinkHref="#play-s"></use>
-            </svg>
-            <span>Play</span>
-          </button>
+          {!isPlaying ?
+            createPlayerControlButton(PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT, `#play-s`, `Play`) :
+            createPlayerControlButton(PAUSE_BUTTON_WIDTH, PAUSE_BUTTON_HEIGHT, `#pause`, `Pause`)
+          }
           <div className="player__name">{name}</div>
 
-          <button type="button" className="player__full-screen">
-            <svg viewBox="0 0 27 27" width="27" height="27">
-              <use xlinkHref="#full-screen"></use>
-            </svg>
-            <span>Full screen</span>
-          </button>
+          {createPlayerControlButton(
+              FULL_SCREEN_BUTTON_WIDTH,
+              FULL_SCREEN_BUTTON_HEIGHT,
+              `#full-screen`,
+              `Full screen`,
+              `player__full-screen`
+          )}
         </div>
       </div >
     </div >
