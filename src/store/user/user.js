bc0b8b44ac39606d43420720d298e5db911b9e80
Kostya Kaganov <kkaganov@gmail.com>
2021-03-21 13:08:08 +0200

    Add loadUser to user reducer

--- src/store/user/user.js
+++ src/store/user/user.js
@@ -1,18 +1,19 @@
 import {createReducer} from "@reduxjs/toolkit";
 import {AuthorizationStatus} from "../../util/const";
-import {requireAuthorization} from "../action";
+import {requireAuthorization, loadUser} from "../action";
 
 const initialState = {
-  id: null,
-  email: null,
-  name: null,
-  avatarUrl: null,
+  user: null,
   authorizationStatus: AuthorizationStatus.NO_AUTH
 };
 
 const user = createReducer(initialState, (builder) => {
   builder.addCase(requireAuthorization, (state, action) => {
     state.authorizationStatus = action.payload;
+  });
+
+  builder.addCase(loadUser, (state, action) => {
+    state.user = action.payload;
   });
 });
 
