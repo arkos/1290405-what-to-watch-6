ecb39275c00fb98836e4b3c1922a29c20a4772f0
Kostya Kaganov <kkaganov@gmail.com>
2021-03-26 23:36:09 +0300

    Add tests to movieData reducer

--- src/store/movie-data/movie-data.test.js
+++ src/store/movie-data/movie-data.test.js
@@ -2,7 +2,7 @@ import MockAdapter from 'axios-mock-adapter';
 import {createAPI} from '../../services/api';
 import {APIRoute} from '../../util/const';
 import {getPromoMovieUrl} from '../../util/route';
-import {loadMovies, loadPromo, loadReviews, reloadMovie} from '../action';
+import {addFavorite, loadFavorites, loadMovies, loadPromo, loadReviews, reloadMovie, saveReview} from '../action';
 import {fetchMovies, fetchPromo} from '../api-actions';
 import {movieData} from './movie-data';
 
@@ -153,66 +153,158 @@ describe(`Reducer 'movieData should work correctly'`, () => {
           [expect.objectContaining({reviews: sampleReviews})]), isDataLoaded: true}
       );
   });
-});
 
-it(`Reducer should update reviews by loadReviews even if there are no reviews`, () => {
-  const sampleMovies = [
-    {
-      name: `Second`,
-      id: 2
-    },
-    {
-      name: `First`,
-      id: 1
-    },
-  ];
+  it(`Reducer should update reviews by loadReviews even if there are no reviews`, () => {
+    const sampleMovies = [
+      {
+        name: `Second`,
+        id: 2
+      },
+      {
+        name: `First`,
+        id: 1
+      },
+    ];
 
-  const [, sampleMovie] = sampleMovies;
+    const [, sampleMovie] = sampleMovies;
 
-  const state = {movies: sampleMovies, isDataLoaded: true};
+    const state = {movies: sampleMovies, isDataLoaded: true};
 
-  const sampleReviews = [];
+    const sampleReviews = [];
 
-  expect(movieData(state, loadReviews(sampleReviews, sampleMovie.id)))
-      .toEqual({movies: expect.arrayContaining(
-          [expect.objectContaining({reviews: sampleReviews})]), isDataLoaded: true}
-      );
-});
+    expect(movieData(state, loadReviews(sampleReviews, sampleMovie.id)))
+        .toEqual({movies: expect.arrayContaining(
+            [expect.objectContaining({reviews: sampleReviews})]), isDataLoaded: true}
+        );
+  });
 
-it(`Reducer should not update movie by reloadMovie if it's null`, () => {
-  const sampleMovies = [
-    {
-      name: `First`,
-      id: 1
-    },
-    {
-      name: `Second`,
-      id: 2
-    },
-  ];
+  it(`Reducer should not update movie by reloadMovie if it's null`, () => {
+    const sampleMovies = [
+      {
+        name: `First`,
+        id: 1
+      },
+      {
+        name: `Second`,
+        id: 2
+      },
+    ];
 
-  const state = {movies: sampleMovies, isDataLoaded: true};
+    const state = {movies: sampleMovies, isDataLoaded: true};
 
-  expect(movieData(state, reloadMovie(null))).toEqual({movies: expect.not.arrayContaining([null]), isDataLoaded: true});
-});
+    expect(movieData(state, reloadMovie(null))).toEqual({movies: expect.not.arrayContaining([null]), isDataLoaded: true});
+  });
 
-it(`Reducer should update movie by reloadMovie if it's not null`, () => {
-  const sampleMovies = [
-    {
-      name: `First`,
-      id: 1
-    },
-    {
-      name: `Second`,
-      id: 2
-    },
-  ];
+  it(`Reducer should update movie by reloadMovie if it's not null`, () => {
+    const sampleMovies = [
+      {
+        name: `First`,
+        id: 1
+      },
+      {
+        name: `Second`,
+        id: 2
+      },
+    ];
 
-  const state = {movies: sampleMovies, isDataLoaded: true};
+    const state = {movies: sampleMovies, isDataLoaded: true};
 
-  const expectedMovie = {name: `Third`, id: 3};
+    const expectedMovie = {name: `Third`, id: 3};
 
-  expect(movieData(state, reloadMovie(expectedMovie))).toEqual({movies: expect.arrayContaining([expectedMovie]), isDataLoaded: true});
+    expect(movieData(state, reloadMovie(expectedMovie))).toEqual({movies: expect.arrayContaining([expectedMovie]), isDataLoaded: true});
+  });
+
+  it(`Reducer should add the movie by reloadMovie if it doesn't exist`, () => {
+    const state = {movies: [], isDataLoaded: true};
+
+    const expectedMovie = {name: `First`, id: 1};
+
+    expect(movieData(state, reloadMovie(expectedMovie)))
+      .toEqual({movies: expect.arrayContaining([expectedMovie]), isDataLoaded: true});
+  });
+
+  it(`Reducer should not add a review for a movie by saveReview if the movie doesn't exist`, () => {
+    const state = {movies: [{id: 2, name: ``, reviews: []}]};
+    const review = {comment: ``};
+    const result = movieData(state, saveReview(review, 1));
+
+    expect(result)
+      .toEqual({movies: expect.arrayContaining([expect.objectContaining({id: 2, name: ``})])});
+
+    expect(result)
+      .toEqual({movies: expect.arrayContaining([expect.not.objectContaining({reviews: expect.arrayContaining([review])})])});
+  });
+
+  it(`Reducer should add a review for a movie by saveReview`, () => {
+    const state = {movies: [{id: 1, name: ``, reviews: []}]};
+    const review = {comment: ``};
+    const result = movieData(state, saveReview(review, 1));
+
+    expect(result)
+      .toEqual({movies: expect.arrayContaining([expect.objectContaining({id: 1, name: ``, reviews: expect.arrayContaining([review])})])});
+  });
+
+  it(`Reducer should not mark a movie as favorite by addFavorite if the movie doesn't exist`, () => {
+    const state = {movies: [{id: 1, name: ``}]};
+    const result = movieData(state, addFavorite({id: 2, name: ``, isFavorite: true}));
+
+    expect(result)
+      .toEqual({movies: expect.arrayContaining([expect.objectContaining({id: 1, name: ``})])});
+
+    expect(result)
+      .toEqual({movies: expect.not.arrayContaining([{id: 1, name: ``, isFavorite: true}])});
+  });
+
+  it(`Reducer should mark a movie as favorite by addFavorite`, () => {
+    const state = {movies: [{id: 1, name: ``}]};
+    const result = movieData(state, addFavorite({id: 1, name: ``, isFavorite: true}));
+
+    expect(result)
+      .toEqual({movies: expect.arrayContaining([expect.objectContaining({id: 1, name: ``, isFavorite: true})])});
+  });
+
+  it(`Reducer should add all favorite movies by loadFavorites if there are no other movies`, () => {
+    const state = {movies: []};
+
+    const favorites = [
+      {id: 1, isFavorite: true},
+      {id: 2, isFavorite: true},
+      {id: 3, isFavorite: true},
+    ];
+
+    expect(movieData(state, loadFavorites(favorites)))
+      .toEqual({movies: expect.arrayContaining([...favorites])});
+  });
+
+  it(`Reducer should add favorite movies by loadFavorites`, () => {
+    const state = {movies: [{id: 1}]};
+
+    const favorites = [
+      {id: 1, isFavorite: true},
+      {id: 2, isFavorite: true},
+      {id: 3, isFavorite: true},
+    ];
+
+    const result = movieData(state, loadFavorites(favorites));
+
+    expect(result)
+      .toEqual({movies: expect.arrayContaining([{id: 1, isFavorite: true}])});
+
+    expect(result)
+      .toEqual({movies: expect.not.arrayContaining([{id: 2, isFavorite: true}])});
+  });
+
+  it(`Reducer should mark a movie as promo by loadPromo`, () => {
+    const state = {
+      movies: [
+        {id: 1},
+        {id: 2},
+        {id: 3},
+      ]};
+
+    expect(movieData(state, loadPromo({id: 1})))
+      .toEqual({movies: expect.arrayContaining([{id: 1, isPromo: true}, {id: 2, isPromo: false}, {id: 3, isPromo: false}])});
+  });
 });
 
 describe(`Async operation should work correctly`, () => {
