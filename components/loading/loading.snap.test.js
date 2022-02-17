a3464ef8a469aff473afbd5daf36c9f952292757
Kostya Kaganov <kkaganov@gmail.com>
2022-02-17 16:20:10 +0200

    Updates

--- components/loading/loading.snap.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import Loading from './loading';
-
-it(`Should Loading render correctly`, () => {
-  const {container} = render(<Loading />);
-
-  expect(container).toMatchSnapshot();
-});
--- util/user.js
+++
@@ -1,8 +0,0 @@
-export const adaptToClient = (user) => {
-  const adaptedUser = Object.assign({}, user, {
-    avatarUrl: user.avatar_url
-  });
-
-  delete adaptedUser.avatar_url;
-  return adaptedUser;
-};
