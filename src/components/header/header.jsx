f3d9da977345a1ede987437a7c02971f3a646f2d
Kostya Kaganov <kkaganov@gmail.com>
2022-04-28 00:24:10 +0300

    Use Logo and SignInIndicator in Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,10 @@
+import Logo from "../logo/logo";
 import SignInIndicator from "../sign-in-indicator/sign-in-indicator";
 
-const Header = ({ className }) => {
+const Header = ({ className, href }) => {
   return (
     <header className={`page-header ${className}`}>
-      <div className="logo">
-        <a className="logo__link">
-          <span className="logo__letter logo__letter--1">W</span>
-          <span className="logo__letter logo__letter--2">T</span>
-          <span className="logo__letter logo__letter--3">W</span>
-        </a>
-      </div>
-
+      <Logo href={href} />
       <SignInIndicator />
     </header>
   );
