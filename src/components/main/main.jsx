9cb1d675745d5e45bf5e55a8ed273aa0a024951a
Kostya Kaganov <kkaganov@gmail.com>
2022-04-28 09:00:16 +0300

    Replace static HTML with Promo

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,47 +1,30 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  getFilteredMovies,
-  getPromoMovie,
-} from "../../store/selectors/selectors";
-import { FavoriteStatus, MOVIES_PER_PAGE, StateStatus } from "../../util/const";
-import { getPlayerUrl } from "../../util/route";
+import { getFilteredMovies } from "../../store/selectors/selectors";
+import { MOVIES_PER_PAGE, StateStatus } from "../../util/const";
 import MovieList from "../movie-list/movie-list";
 import GenreList from "../genre-list/genre-list";
 import Loading from "../loading/loading";
 import ShowMore from "../show-more/show-more";
-import { fetchMovies, postFavorite, fetchPromo } from "../../store/api-actions";
+import { fetchMovies } from "../../store/api-actions";
 import { changeCountToRender, resetMain } from "../../store/action";
-import AddFavorite from "../add-favorite/add-favorite";
-import Header from "../header/header";
+import Promo from "../promo/promo";
 
 const Main = () => {
   let movies = useSelector((state) => getFilteredMovies(state));
   const { status: statusMovies } = useSelector((state) => state.MOVIES);
   const { renderedMoviesCount } = useSelector((state) => state.MOVIE);
 
-  const { status: statusPromo } = useSelector((state) => state.PROMO);
-  const promo = useSelector((state) => getPromoMovie(state));
-
   const filteredMoviesCount = movies.length;
   movies = movies.slice(0, renderedMoviesCount);
 
   const dispatch = useDispatch();
-
-  const history = useNavigate();
 
   useEffect(() => {
     if (statusMovies === StateStatus.WORKING) {
       dispatch(fetchMovies());
     }
   }, [statusMovies, dispatch]);
-
-  useEffect(() => {
-    if (statusPromo === StateStatus.WORKING) {
-      dispatch(fetchPromo());
-    }
-  }, [statusPromo, dispatch]);
 
   useEffect(() => {
     dispatch(resetMain());
@@ -55,71 +38,13 @@ const Main = () => {
     );
   };
 
-  const addToFavorite = () => {
-    if (promo) {
-      dispatch(postFavorite(promo.id, FavoriteStatus.FAVORITE));
-    }
-  };
-
-  if (
-    statusMovies === StateStatus.WORKING ||
-    statusPromo === StateStatus.WORKING
-  ) {
+  if (statusMovies === StateStatus.WORKING) {
     return <Loading />;
   }
 
-  const { id, name, backgroundImagePath, posterImagePath, genre, released } =
-    promo;
-
   return (
     <React.Fragment>
-      <section className="movie-card">
-        <div className="movie-card__bg">
-          <img src={backgroundImagePath} alt={name} />
-        </div>
-
-        <h1 className="visually-hidden">WTW</h1>
-
-        <Header className="movie-card__head" />
-        <div className="movie-card__wrap">
-          <div className="movie-card__info">
-            <div className="movie-card__poster">
-              <img
-                src={posterImagePath}
-                alt={`${name} poster`}
-                width="218"
-                height="327"
-              />
-            </div>
-
-            <div className="movie-card__desc">
-              <h2 className="movie-card__title">{name}</h2>
-              <p className="movie-card__meta">
-                <span className="movie-card__genre">{genre}</span>
-                <span className="movie-card__year">{released}</span>
-              </p>
-
-              <div className="movie-card__buttons">
-                <button
-                  className="btn btn--play movie-card__button"
-                  type="button"
-                  onClick={() => history(getPlayerUrl(id))}
-                >
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <AddFavorite
-                  onAddToFavorite={addToFavorite}
-                  favoriteStatus={promo.isFavorite}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
+      <Promo />
       <div className="page-content">
         <section className="catalog">
           <h2 className="catalog__title visually-hidden">Catalog</h2>
