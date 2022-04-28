df1484397c6120f00f366e8fcb91f37a10438a00
Kostya Kaganov <kkaganov@gmail.com>
2022-04-28 08:39:32 +0300

    Allow children in Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,17 @@
 import Logo from "../logo/logo";
 import SignInIndicator from "../sign-in-indicator/sign-in-indicator";
 
-const Header = ({ className, href }) => {
+const Header = ({ className, href, children }) => {
   return (
     <header className={`page-header ${className}`}>
-      <Logo href={href} />
-      <SignInIndicator />
+      {children ? (
+        children
+      ) : (
+        <>
+          <Logo href={href} />
+          <SignInIndicator />
+        </>
+      )}
     </header>
   );
 };
